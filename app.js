3430b705bc2a9b81b6fc6b39dc632ed225d3fe0d
Ricardo Baglio <rbaglio@produpress.be>
2019-05-14 11:30:48 +0200

    Update

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 const config = require("config");
+const helmet = require("helmet");
+
+app.use(helmet());
+
+app.use('/files', express.static(__dirname + '/public'));
+app.set('view engine', 'pug')
+app.set('views', './views');
 
 app.get("/", (request, response) => {
 
-    response.send('ok');
+    response.render('index', {
+
+        title: `Hello World`
+        
+    });
 
 });
 
